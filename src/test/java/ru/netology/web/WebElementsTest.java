1ac9513a4afd3c85789dc2e9a8d05ecf87bc3572
Дмитрий <1917-01_1917@mail.ru>
2022-05-02 15:49:24 +0400

    added tests for the second task

--- src/test/java/ru/netology/web/WebElementsTest.java
+++ src/test/java/ru/netology/web/WebElementsTest.java
@@ -65,5 +65,83 @@ public class WebElementsTest {
         assertEquals("Я соглашаюсь с условиями обработки и использования моих персональных данных и разрешаю сделать запрос в бюро кредитных историй", text.trim());
     }
 
+    @Test
+    public void shouldBeACheckOfWritingOnlyRussianLettersSpacesAndHyphens() {
+        driver.get("http://localhost:9999");
+        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Petrov Ivan");
+        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79277656556");
+        driver.findElement(By.cssSelector("[data-test-id=agreement]")).click(); // чекбокс
+        driver.findElement(By.tagName("button")).click(); // Отправить
+
+        String text = driver.findElement(By.cssSelector("[data-test-id=name].input_invalid .input__sub")).getText();
+        assertEquals("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.", text.trim());
+    }
+
+    @Test
+    public void shouldNotBeEmptyForm() {
+        driver.get("http://localhost:9999");
+        driver.findElement(By.tagName("button")).click();
+
+        String text = driver.findElement(By.cssSelector("[data-test-id=name].input_invalid .input__sub")).getText();
+        assertEquals("Поле обязательно для заполнения", text.trim());
+    }
+
+    @Test
+    public void shouldNotEmptyNameField() {
+        driver.get("http://localhost:9999");
+        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79277656556");
+        driver.findElement(By.cssSelector("[data-test-id=agreement]")).click(); // чекбокс
+        driver.findElement(By.tagName("button")).click(); // Отправить
+
+        String text = driver.findElement(By.cssSelector("[data-test-id=name].input_invalid .input__sub")).getText();
+        assertEquals("Поле обязательно для заполнения", text.trim());
+    }
+
+    @Test
+    public void shouldNotEmptyPhoneField() {
+        driver.get("http://localhost:9999");
+        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Петров Иван");
+        driver.findElement(By.cssSelector("[data-test-id=agreement]")).click(); // чекбокс
+        driver.findElement(By.tagName("button")).click(); // Отправить
+
+        String text = driver.findElement(By.cssSelector("[data-test-id=phone].input_invalid .input__sub")).getText();
+        assertEquals("Поле обязательно для заполнения", text.trim());
+    }
+
+    @Test
+    public void shouldFirstNameFieldValidationDash() {
+        driver.get("http://localhost:9999");
+        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Кириллова Алла-Виктория");
+        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79206161994");
+        driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
+        driver.findElement(By.className("button")).click();
+
+        String text = driver.findElement(By.cssSelector("[data-test-id=order-success]")).getText();
+        assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text.trim());
+    }
+
+    @Test
+    public void shouldLastNameFieldValidationDash() {
+        driver.get("http://localhost:9999");
+        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Кириллова-Деникина Алла");
+        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79206161994");
+        driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
+        driver.findElement(By.className("button")).click();
+
+        String text = driver.findElement(By.cssSelector("[data-test-id=order-success]")).getText();
+        assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text.trim());
+    }
+
+    @Test
+    public void shouldFirstAndLastNameFieldValidationDash() {
+        driver.get("http://localhost:9999");
+        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Кириллова-Деникина Алла-Виктория");
+        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79206161994");
+        driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
+        driver.findElement(By.className("button")).click();
+
+        String text = driver.findElement(By.cssSelector("[data-test-id=order-success]")).getText();
+        assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text.trim());
+    }
 
 }
